05515115eed1ea0afd39ab7ef63d4d6566b0cba2
patrickaustin <pja9386@nyu.edu>
2023-02-03 14:02:58 -0500

    Lab1

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,7 +5,7 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    int()
+
     if question == "Are encoding and encryption the same? - Yes/No":
         return "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
