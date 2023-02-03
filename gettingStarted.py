c56f61dc9639cedcd2d96fe9b8f270c50540719c
patrickaustin <pja9386@nyu.edu>
2023-02-03 13:55:07 -0500

    Lab1

--- gettingStarted.py
+++ gettingStarted.py
@@ -48,21 +48,6 @@ print(welcome_assignment_answers("What layer of the TCP/IP model does the protoc
 # Complete all the questions.
 
 
-###def switch(lang):
-#    if lang == "JavaScript":
- #       return "You can become a web developer."
-  #  elif lang == "PHP":
-   #     return "You can become a backend developer."
-    #elif lang == "Python":
-     #   return "You can become a Data Scientist"
-    #elif lang == "Solidity":
-     #   return "You can become a Blockchain developer."
-    #elif lang == "Java":
-     #   return "You can become a mobile app developer"
-
-#print(switch("JavaScript"))
-#print(switch("PHP"))
-#print(switch("Java"))
 
 
 #if __name__ == "__main__":
