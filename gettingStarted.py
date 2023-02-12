8662401c2d048a23fc3dc33754d525b03eea4415
patrickaustin <pja9386@nyu.edu>
2023-02-12 16:40:44 -0500

    Lab1

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,15 +10,15 @@ def welcome_assignment_answers(question):
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         return "mTCP"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        return "No"
+        return "Yes"
     elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
         return "883c13da6a24949c9a23231b60119e2ace58459da4f8bbdd812cc37764548bdd"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         return "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        return int(4)
+        return int(2)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        return int(1)
+        return int(3)
     else:
         return 'The answer could not be determined'
 
