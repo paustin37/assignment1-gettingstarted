0cc273e825df8dc99b006725cdd4255195cf4deb
patrickaustin <pja9386@nyu.edu>
2023-02-12 15:42:13 -0500

    Lab1

--- gettingStarted.py
+++ gettingStarted.py
@@ -23,14 +23,17 @@ def welcome_assignment_answers(question):
         return 'The answer could not be determined'
 
 
-print(welcome_assignment_answers("Are encoding and encryption the same? - Yes/No"))
-print(welcome_assignment_answers("Is it possible to decrypt a message without a key? - Yes/No"))
-print(welcome_assignment_answers("Is it possible to decode a message without a key? - Yes/No"))
-print(welcome_assignment_answers("In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"))
-print(welcome_assignment_answers("Is a hashed message supposed to be un-hashed? - Yes/No"))
-print(welcome_assignment_answers("What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code"))
-print(welcome_assignment_answers("Is MD5 a secured hashing algorithm? - Yes/No"))
-print(welcome_assignment_answers("What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"))
-print(welcome_assignment_answers("What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"))
+#print(welcome_assignment_answers("Are encoding and encryption the same? - Yes/No"))
+#print(welcome_assignment_answers("Is it possible to decrypt a message without a key? - Yes/No"))
+#print(welcome_assignment_answers("Is it possible to decode a message without a key? - Yes/No"))
+#print(welcome_assignment_answers("In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"))
+#print(welcome_assignment_answers("Is a hashed message supposed to be un-hashed? - Yes/No"))
+#print(welcome_assignment_answers("What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code"))
+#print(welcome_assignment_answers("Is MD5 a secured hashing algorithm? - Yes/No"))
+#print(welcome_assignment_answers("What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"))
+#print(welcome_assignment_answers("What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"))
 
-
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
