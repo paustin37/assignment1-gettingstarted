fb0731367982e844677b78ad650144ac364deedd
patrickaustin <pja9386@nyu.edu>
2023-02-12 16:28:34 -0500

    Lab1

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,7 +16,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         return "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        question8: int = 4
+        question8 = 4
         return int(question8)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         return int(3)
